91fb342054f92f853c5effc4892da710cdce76e9
Carlitos <Carlitos@DESKTOP-9611RRB>
2022-04-16 11:12:29 -0600

    Se actualiza con tarea S12. Proyecto no está levantando, por lo que se creará repositorio extra para colocar el proyecto funcional.

--- src/main/java/com/tienda/controller/PersonasController.java
+++ src/main/java/com/tienda/controller/PersonasController.java
@@ -7,8 +7,21 @@ package com.tienda.controller;
 // java.util;
 import com.tienda.entity.Persona;
 import com.tienda.service.iPersonaService;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
 
 import java.util.List;
+import java.util.Map;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.util.JRLoader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +29,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  *
@@ -53,6 +67,35 @@ public class PersonasController {
         personaService.delete(idPersona);
         return "redirect:/personas";
     }
+    
+@RequestMapping("/pdf")
+
+    public void getReportsinPDF(HttpServletResponse response) throws JRException, IOException {
+
+        //Compiled report
+        InputStream jasperStream = (InputStream) this.getClass().getResourceAsStream("/reports/Reporte_Empleados.jasper");
+
+        //Adding attribute names
+        Map params = new HashMap<>();
+        params.put("id", "id");
+        params.put("nombre", "nombre");
+        params.put("apellido1", "apellido1");
+        params.put("apellido2", "apellido2");
+        params.put("roles", "roles");
+        
+
+        // Fetching the student from the data database.
+        final JRBeanCollectionDataSource source = new JRBeanCollectionDataSource(personaService.getAllPerson());
+
+        JasperReport jasperReport = (JasperReport) JRLoader.loadObject(jasperStream);
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, source);
+
+        response.setContentType("application/x-pdf");
+        response.setHeader("Content-disposition", "inline; filename=ListaPersonas.pdf");
+
+        final ServletOutputStream outStream = response.getOutputStream();
+        JasperExportManager.exportReportToPdfStream(jasperPrint, outStream);
+    }    
 }
 
 /*Protocolos
